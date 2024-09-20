7b3d278f332846b485bd052a4c9ae83c3d597fec
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-09-20 13:00:27 +0300

    update

--- chatterBox-Client/chatter-box/src/Components/Groups.jsx
+++ chatterBox-Client/chatter-box/src/Components/Groups.jsx
@@ -4,56 +4,72 @@ import Logo from'../assets/Logo.png'
 import { IconButton } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 import { useDispatch, useSelector } from "react-redux";
+import { AnimatePresence,  motion } from "framer-motion"
 
 const Groups = () => {
   
   const lightTheme = useSelector((state) => state.themeKey);
 
   return (
-    <div className="list-container">
-    <div className={"ug-header" + ((lightTheme)?' ':' dark') }>
-        <img src={Logo} alt=""
-             style={{height: "3rem", width:"4rem"}} />
-        <p className='ug-title'>Available Groups</p>
-    </div>
+    <AnimatePresence>
+        <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{
+        ease: "anticipate",
+        duration: "0.3",
+        }} 
+        className="list-container">
+            <div className={"ug-header" + ((lightTheme)?' ':' dark') }>
+                <img src={Logo} alt=""
+                    style={{height: "3rem", width:"4rem"}} />
+                <p className='ug-title'>Available Groups</p>
+            </div>
 
-    <div className={"sb-search" + ((lightTheme)?' ':' dark') }>
-        <IconButton>
-            <SearchIcon/>
-        </IconButton>
+            <div className={"sb-search" + ((lightTheme)?' ':' dark') }>
+                <IconButton>
+                    <SearchIcon/>
+                </IconButton>
 
-        <input className='search-box' placeholder='Search for a group' type="text" />
-    </div>
+                <input className='search-box' placeholder='Search for a group' type="text" />
+            </div>
 
-    <div className={"ug-list" + ((lightTheme)?' ':' dark') }>
-        <div className="list-tem">
-            <p className='con-icon'>G</p>
-            <p className='con-Title'>Group#1</p>
-        </div>
+            <motion.div className={"ug-list" + ((lightTheme)?' ':' dark') }
+            
+            whileHover={{ scale: 1.01 }}
+            whileTap={{ scale: 1 }}
+            >
 
-        <div className="list-tem">
-            <p className='con-icon'>G</p>
-            <p className='con-Title'>Group#2</p>
-        </div>
+                <div className="list-tem">
+                    <p className='con-icon'>G</p>
+                    <p className='con-Title'>Group#1</p>
+                </div>
 
-        <div className="list-tem">
-            <p className='con-icon'>G</p>
-            <p className='con-Title'>Group#3</p>
-        </div>
+                <div className="list-tem">
+                    <p className='con-icon'>G</p>
+                    <p className='con-Title'>Group#2</p>
+                </div>
 
-        <div className="list-tem">
-            <p className='con-icon'>G</p>
-            <p className='con-Title'>Group#4</p>
-        </div>
+                <div className="list-tem">
+                    <p className='con-icon'>G</p>
+                    <p className='con-Title'>Group#3</p>
+                </div>
 
-        <div className="list-tem">
-            <p className='con-icon'>G</p>
-            <p className='con-Title'>Group#5</p>
-        </div>
+                <div className="list-tem">
+                    <p className='con-icon'>G</p>
+                    <p className='con-Title'>Group#4</p>
+                </div>
 
-        </div>
-    </div>
+                <div className="list-tem">
+                    <p className='con-icon'>G</p>
+                    <p className='con-Title'>Group#5</p>
+                </div>
 
+            </motion.div>
+
+        </motion.div>
+    </AnimatePresence>
   )
 }
 
