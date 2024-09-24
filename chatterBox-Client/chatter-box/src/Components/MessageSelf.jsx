b3e4376a1ebd5452afc94adbb7c770cd3f020e9c
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-09-24 09:32:09 +0300

    messageSelf Content Update

--- chatterBox-Client/chatter-box/src/Components/MessageSelf.jsx
+++ chatterBox-Client/chatter-box/src/Components/MessageSelf.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const MessageSelf = () => {
-  var props2 = {content:"Let's talk numbers"}
+
   return (
     <div className="self-message-container">
         <div className="messageBox">
-        <p style={{ color: "black" }}>{props2.content}</p>
+        <p style={{ color: "black" }}>{props.content}</p>
         {/* <p className="self-timeStamp" style={{ color: "black" }}>
             12:00am
         </p> */}
